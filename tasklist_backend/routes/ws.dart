8daa2ca9ab5ca89616d3befab5e4999827cf905b
AhmedSamy02 <nname1858@gmail.com>
2024-07-09 02:42:38 +0300

    [➕ FR] Backend; Chat websocket added

---
+++ tasklist_backend/routes/ws.dart
@@ -0,0 +1,18 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
+import 'package:tasklist_backend/constants/values.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  final handler = webSocketHandler(
+    (channel, protocol) {
+      logger.d('Connected to WebSocket channel: $channel');
+
+      channel.stream.listen((message) {
+        logger.d('Received message: $message');
+        channel.sink.add(message);
+      });
+    },
+  );
+
+  return handler(context);
+}
