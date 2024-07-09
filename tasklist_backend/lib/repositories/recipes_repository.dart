f6b0c9a996e9e4661697acb1589949b7a9de3b3c
AhmedSamy02 <nname1858@gmail.com>
2024-07-09 22:04:36 +0300

    [🎨 REF] Backend; Refactoring Recipies GET API

--- tasklist_backend/lib/repositories/recipes_repository.dart
+++ tasklist_backend/lib/repositories/recipes_repository.dart
@@ -1,3 +1,17 @@
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:tasklist_backend/constants/methods.dart';
+import 'package:tasklist_backend/constants/values.dart';
+
 class RecipesRepository {
-  
+  Future<List<Map<String, dynamic>>> getRecipies(int page, int limit) async {
+    final data = await getIt
+        .get<DbCollection>(instanceName: 'recipes')
+        .find(where.skip(limit * abs(page)).limit(limit))
+        .toList();
+    for (final element in data) {
+      final id = element['_id'] as ObjectId;
+      element['_id'] = id.oid;
+    }
+    return data;
+  }
 }
--- tasklist_backend/routes/recipes/index.dart
+++ tasklist_backend/routes/recipes/index.dart
@@ -5,6 +5,7 @@ import 'package:mongo_dart/mongo_dart.dart';
 import 'package:tasklist_backend/constants/methods.dart';
 import 'package:tasklist_backend/constants/responses.dart';
 import 'package:tasklist_backend/constants/values.dart';
+import 'package:tasklist_backend/repositories/recipes_repository.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   if (context.request.method != HttpMethod.get) {
@@ -17,14 +18,9 @@ Future<Response> onRequest(RequestContext context) async {
   final pageNumber = context.request.url.queryParameters['offset'] == null
       ? 0
       : int.parse(context.request.url.queryParameters['offset']!);
-  final data = await getIt
-      .get<DbCollection>(instanceName: 'recipes')
-      .find(where.skip(pageLimit * abs(pageNumber)).limit(pageLimit))
-      .toList();
-  for (final element in data) {
-    final id = element['_id'] as ObjectId;
-    element['_id'] = id.oid;
-  }
+  final data = await context
+      .read<RecipesRepository>()
+      .getRecipies(pageNumber, pageLimit);
   return successResponseWithPage(
     'Recipes Got Successfully',
     data,
